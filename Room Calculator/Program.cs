6650eb171cfef8e0e1c6c919f2d70c955e520e02
reidmedlock <benmedlock19@gmail.com>
2023-05-03 11:27:32 -0400

    Add project files.

---
+++ Room Calculator/Program.cs
@@ -0,0 +1,20 @@
+﻿static int calculateArea(int lengthInFeets, int widthInFeets)
+{
+    return (lengthInFeets * widthInFeets);
+}
+
+{
+    int lengthInFeets = 0;
+    int widthInFeets = 0;
+    int plotArea = 0;
+
+
+    Console.Write("Enter length of the plot in feets: ");
+    lengthInFeets = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter width of room in feet:");
+    widthInFeets = Convert.ToInt32(Console.ReadLine());
+
+    plotArea = calculateArea(lengthInFeets, widthInFeets);
+
+    Console.WriteLine("Plot area is " + plotArea + " square feet");
